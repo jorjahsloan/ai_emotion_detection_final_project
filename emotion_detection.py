050cc3bc12993ea28cd4a91f4859719ce833b78c
jorjahsloan <jorjah.sloan@gmail.com>
2025-11-23 10:58:28 -0500

    extracting emotions in json wip

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,5 +6,26 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myobj, headers=header)
+    formatted_response = json.loads(response.text)
+
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+
+    #print(f"{emotion}")
+
+    #print(f"{formatted_response=}\n")
+    #print(f"{formatted_response['emotionPredictions']=}\n")
+    #print(f"{formatted_response['emotionPredictions'][0]=}\n")
+    #print(f"{formatted_response['emotionPredictions'][0]['emotion']=}\n")
+
+
+    anger = emotion['anger']
+    digust = emotion['disgust']
+    fear = emotion['fear']
+    joy = emotion['joy']
+    sadness = emotion['sadness']  
+
+    print(f"{anger}")
+
     return response.text
+
 
