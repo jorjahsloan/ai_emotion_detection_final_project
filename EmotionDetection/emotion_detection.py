219dfa0e64ac5b27a2d4bac00a57979505e20b74
jorjahsloan <jorjah.sloan@gmail.com>
2025-11-23 12:12:35 -0500

    packaging

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,14 +2,25 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    #Define the URL for the emotion detection API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    #Set the headers with the correct model ID for the API
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    #Create the payload with the text to be analysed 
     myobj = { "raw_document": { "text": text_to_analyze } }
+
+    #Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
+
+    #Parse the response from the API
     formatted_response = json.loads(response.text)
 
+    #Extract the emotions 
     emotion = formatted_response['emotionPredictions'][0]['emotion']
     
+    #Identify the dominant emotion
     emotion['dominant_emotion'] = max(emotion, key=emotion.get)
 
     return emotion
